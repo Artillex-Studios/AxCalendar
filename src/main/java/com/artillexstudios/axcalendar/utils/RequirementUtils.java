e1326d636287804ed503bb73e0eefde14ede19bf
BenceX100 <52270269+BenceX100@users.noreply.github.com>
2023-12-01 19:00:29 +0100

    Small fix

--- src/main/java/com/artillexstudios/axcalendar/utils/RequirementUtils.java
+++ src/main/java/com/artillexstudios/axcalendar/utils/RequirementUtils.java
@@ -11,20 +11,26 @@ public class RequirementUtils {
     public static boolean canClaim(@NotNull Player player) {
         if (CONFIG.getStringList("claim-requirements") == null || CONFIG.getStringList("claim-requirements").isEmpty()) return true;
 
+        boolean canClaim = false;
+
         for (String str : CONFIG.getStringList("claim-requirements")) {
             final String[] ar = str.split(" ");
 
             switch (ar[0]) {
                 case "[PLAYTIME]" -> {
-                    return (player.getStatistic(Statistic.PLAY_ONE_MINUTE) / 60 / 20) >= Integer.parseInt(ar[1]);
+                    if ((player.getStatistic(Statistic.PLAY_ONE_MINUTE) / 60 / 20) >= Integer.parseInt(ar[1])) {
+                        canClaim = true;
+                    }
                 }
 
                 case "[PERMISSION]" -> {
-                    return player.hasPermission(ar[1]);
+                    if (player.hasPermission(ar[1])) {
+                        canClaim = true;
+                    }
                 }
             }
         }
 
-        return false;
+        return canClaim;
     }
 }
